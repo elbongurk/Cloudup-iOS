b3966440408d26a3b1df5a7dbaf45bd95cc27fd6
keegnotrub <ryan@elbongurk.com>
2014-04-22 15:27:25 -0500

    Initial Commit

---
+++ Cloudup-iOS/Classes/Delegates/EGKAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  EGKAppDelegate.m
+//  Cloudup-iOS
+//
+//  Created by Ryan Krug on 4/22/14
+//  Copyright (c) 2014 Elbongurk. All rights reserved.
+//
+
+#import "EGKAppDelegate.h"
+
+@implementation EGKAppDelegate
+
+@end
---
+++ Cloudup-iOS/Resources/Other-Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cloudup-iOS
+//
+//  Created by Ryan Krug on 4/22/14
+//  Copyright (c) 2014 Elbongurk. All rights reserved.
+//
+
+@import UIKit;
+
+#import "EGKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EGKAppDelegate class]));
+    }
+}
